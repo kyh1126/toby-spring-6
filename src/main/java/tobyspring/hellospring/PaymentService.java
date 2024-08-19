b109514c2617e534dd4799fdf5e32e5d06c11ce2
kyh1126 <kyh901126@nate.com>
2024-08-19 20:41:02 +0900

    [1. 스프링 개발 시작하기] PaymentService 개발 (3) - 환율 가져오기

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,18 +1,40 @@
 package tobyspring.hellospring;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class PaymentService {
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
         // 환율 가져오기
+        // https://open.er-api.com/v6/latest/USD
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        System.out.println(response);
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        BigDecimal exRate = data.rates().get("KRW");
+        System.out.println(exRate);
+
         // 금액 계산
         // 유효 시간 계산
         return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO,
                 BigDecimal.ZERO, LocalDateTime.now());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
