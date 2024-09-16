ec75355226228e61e206dbcfc2b3b5c9d3cabacd
kyh1126 <kyh901126@nate.com>
2024-09-16 21:27:51 +0900

    [4. 템플릿] 스프링이 제공하는 템플릿 - 스프링이 제공하는 템플릿 중 제일 많이 쓰는 RestTemplate로 변경해보자

--- src/main/java/tobyspring/hellospring/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -2,10 +2,11 @@ package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import tobyspring.hellospring.api.ApiTemplate;
 import tobyspring.hellospring.api.ErApiExRateExtractor;
 import tobyspring.hellospring.api.SimpleApiExecutor;
-import tobyspring.hellospring.exrate.WebApiExRateProvider;
+import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
 import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.payment.PaymentService;
 
@@ -24,8 +25,13 @@ public class PaymentConfig {
     }
 
     @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider(apiTemplate());
+        return new RestTemplateExRateProvider(restTemplate());
     }
 
     @Bean
