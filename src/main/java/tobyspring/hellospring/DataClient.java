23bd2b34abd1b97ec95c44d9c224d7500819196a
kyh1126 <kyh901126@nate.com>
2024-09-19 01:11:52 +0900

    [4. 템플릿] DataAccessException과 예외 추상화

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -1,7 +1,13 @@
 package tobyspring.hellospring;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 
@@ -11,14 +17,42 @@ public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
         OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-        Order order = new Order("100", BigDecimal.TEN);
-        repository.save(order);
+        try {
+            new TransactionTemplate(transactionManager).execute((TransactionCallback<Order>) status -> {
+                // transaction begin
+                Order order = new Order("100", BigDecimal.TEN);
+                repository.save(order);
 
-        System.out.println(order);
+                System.out.println(order);
 
-        Order order2 = new Order("100", BigDecimal.ONE);
-        repository.save(order2);
+                Order order2 = new Order("100", BigDecimal.ONE);
+                repository.save(order2);
+                // commit
 
+                return null;
+            });
+        } catch (DataAccessException e) {
+            // PersistenceException: JPA Exception
+            // DataIntegrityViolationException: Spring Exception
+            // ConstraintViolationException: Hibernate Exception
+            if (e instanceof DataIntegrityViolationException || e.getCause() instanceof ConstraintViolationException) {
+                System.out.println("주문번호 충돌을 복구하는 작업");
+            } else {
+                System.out.println("DataAccessException - not ConstraintViolationException !!");
+            }
+        }
+
+//        try {
+//            Order order2 = new Order("100", BigDecimal.ONE);
+//            repository.save(order2);
+//        } catch (PersistenceException e) {
+//            if (e.getCause() instanceof ConstraintViolationException) {
+//                System.out.println("주문번호 충돌을 복구하는 작업");
+//            } else {
+//                System.out.println("PersistenceException - not ConstraintViolationException !!");
+//            }
+//        }
     }
 }
