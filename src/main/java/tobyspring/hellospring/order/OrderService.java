258871db182d41fcf1cf1728c9035fda5a6a2f3b
kyh1126 <kyh901126@nate.com>
2024-09-30 00:35:47 +0900

    [5. 서비스 추상화] 트랜잭션 테스트

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -1,11 +1,11 @@
 package tobyspring.hellospring.order;
 
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -23,6 +23,12 @@ public class OrderService {
         return new TransactionTemplate(transactionManager).execute(status -> {
             this.orderRepository.save(order);
             return order;
+        });
+    }
+
+    public List<Order> createOrders(List<OrderReq> reqs) {
+        return new TransactionTemplate(transactionManager).execute(status -> {
+            return reqs.stream().map(req -> createOrder(req.no(), req.total())).toList();
         });
     }
 }
